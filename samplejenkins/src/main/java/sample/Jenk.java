2e6f177a4d3b2338465e338ffd3d2889b69b9083
user <user@DESKTOP-LA3ESVJ>
2023-11-18 16:35:59 +0300

    commit

---
+++ samplejenkins/src/main/java/sample/Jenk.java
@@ -0,0 +1,29 @@
+package sample;
+import java.util.Scanner;
+public class Jenk {
+	
+	
+	
+	
+
+		
+				
+			
+			public static void main(String args[]) 
+			{
+					
+					Scanner reader=new Scanner(System.in);
+					System.out.println("Enter integer number");
+					int a=reader.nextInt();
+					System.out.println("Enter float number");
+					float b=reader.nextFloat();
+					float c=a+b;
+					System.out.println("The sum is"+c);
+				}
+
+			}
+
+
+
+
+
